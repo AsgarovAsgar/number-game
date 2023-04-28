1672eedad3f6612774b9101b99d272f37c82fce0
Asgarov Asgar <asgara@maestro.st>
2023-04-28 17:57:56 +0400

    completed style part

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict'
 console.log('Hello world');
+
+const container = document.querySelector('.container')
+const secretNumber = document.querySelector('.secret_number')
+const guessInput = document.querySelector('.guess')
+
+
+const btnReset = document.querySelector('.reset')
+const btnCheck = document.querySelector('.check')
