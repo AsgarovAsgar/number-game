c59c11fa5bef48d8c81dd978d9ba53c8c585d099
Asgarov Asgar <asgara@maestro.st>
2023-05-05 17:17:32 +0400

    added main logic

--- script.js
+++ script.js
@@ -2,9 +2,46 @@
 console.log('Hello world');
 
 const container = document.querySelector('.container')
-const secretNumber = document.querySelector('.secret_number')
+const secretNumberEl = document.querySelector('.secret_number')
 const guessInput = document.querySelector('.guess')
+const messageEl = document.querySelector('.message')
+const score = document.querySelector('.score')
 
 
 const btnReset = document.querySelector('.reset')
 const btnCheck = document.querySelector('.check')
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+let userScore = 20
+
+secretNumberEl.textContent = secretNumber
+
+// messageEl.textContent = 'akshin'
+console.log(messageEl);
+
+btnCheck.addEventListener('click', function() {
+  const userNumber = +guessInput.value
+
+  if(!guessInput.value) {
+    // console.log('guessInput.value', guessInput.value);
+    messageEl.textContent = 'No Number'
+  }
+
+  if(userNumber < secretNumber) {
+    messageEl.textContent = 'Too Low'
+    userScore--
+    score.textContent = userScore
+  }
+
+  if(userNumber > secretNumber) {
+    messageEl.textContent = 'Too High'
+    userScore--
+    score.textContent = userScore
+  }
+
+  if(userNumber === secretNumber) {
+    messageEl.textContent = 'Correct Number!'
+    container.style.backgroundColor = 'green'
+  }
+
+})
